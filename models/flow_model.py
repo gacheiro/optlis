4d2cdb3b0af00a14ffdda15a0b4adca797d68187
thiagojobson <thiagojobson@gmail.com>
2021-02-05 20:33:40 +0100

    Remove internal generators

--- models/flow_model.py
+++ models/flow_model.py
@@ -41,57 +41,49 @@ T = 3
 def objective_function():
     """The objective function."""
     R = sum(r.values()) * T
-    return "min " + " ".join(f"- {r[i]} z{i}.{t}" for i in D for t in range(1, T+1)) + f" + {R}"
+    return ("min "
+            + " ".join(f"- {r[i]} z{i}.{t}" for i in D for t in range(1, T+1))
+            + f" + {R}")
 
 
 def eq_nb_wt(id=2):
     """Equations to ensure that the maximum number of WT is respected, from
        the origins.
     """
-    def _gen():
-        for c, i in enumerate(O):
-            arc_from_origins = [a for a in A if a[0] == i]
-            for t in range(1, T+1):
-                yield (f"c{id}.{t}.{c}: "
-                       + " + ".join(f"y{i}.{j}.{t}" for i, j in arc_from_origins)
-                       + f" <= {q[i]}")
-
-    return "\n".join(_gen())
+    for c, i in enumerate(O):
+        arc_from_origins = [a for a in A if a[0] == i]
+        for t in range(1, T+1):
+            yield (f"c{id}.{t}.{c}: "
+                    + " + ".join(f"y{i}.{j}.{t}" for i, j in arc_from_origins)
+                    + f" <= {q[i]}")
 
 
 def eq_wt_flow(id=3):
     """Equations to ensure the flow conservation of WT."""
-
-    def _gen():
-        for c, j in enumerate(D):
-            incoming = [a for a in A if a[1] == j]
-            outgoing = [a for a in A if a[0] == j]
-            for t in range(1, T+1):
-                yield (f"c{id}.{t}.{c}: "
-                       + " + ".join(f"y{i}.{j}.{t}" for i, j in incoming)
-                       + "".join(f" - y{j}.{i}.{t}" for j, i in outgoing)
-                       + f" - x{j}.{t} = 0")
-
-    return "\n".join(_gen())
+    for c, j in enumerate(D):
+        incoming = [a for a in A if a[1] == j]
+        outgoing = [a for a in A if a[0] == j]
+        for t in range(1, T+1):
+            yield (f"c{id}.{t}.{c}: "
+                    + " + ".join(f"y{i}.{j}.{t}" for i, j in incoming)
+                    + "".join(f" - y{j}.{i}.{t}" for j, i in outgoing)
+                    + f" - x{j}.{t} = 0")
 
 
 def eq_repear(id=4):
     """Equations to ensure that every node gets the exacly amount of work it needs.
        Setting the z_i^t properly.
     """
-    def _gen():
-        for c, i in enumerate(D):
-            for t in range(1, T+1):
-                yield (f"c{id}.{t}.{c}: {p[i]} z{i}.{t}"
-                       + "".join(f" - x{i}.{tt}" for tt in range(1, t+1))
-                       + " <= 0")
-
-    return "\n".join(_gen())
+    for c, i in enumerate(D):
+        for t in range(1, T+1):
+            yield (f"c{id}.{t}.{c}: {p[i]} z{i}.{t}"
+                    + "".join(f" - x{i}.{tt}" for tt in range(1, t+1))
+                    + " <= 0")
 
 
 def eq_repeared(id=5):
     """Equations to ensure that all nodes are cleaned at the last period."""
-    return f"c{id}: " + " + ".join(f"z{i}.{T}" for i in D) + f" = {len(D)}"
+    yield f"c{id}: " + " + ".join(f"z{i}.{T}" for i in D) + f" = {len(D)}"
 
 
 """
@@ -110,31 +102,27 @@ def c7():
 
 
 def vars():
-    """Model variables."""
-    
-    def _gen():
-        yield "bounds\n"
-        yield "\n".join(f"y{i}.{j}.{t} >= 0" for t in range(1, T+1)
-                                             for i, j in A)
-        yield "\nbinaries\n"
-        yield "\n".join(f"x{i}.{t}" for t in range(1, T+1)
-                                    for i in D)
-        yield ""
-        yield "\n".join(f"z{i}.{t}" for t in range(1, T+1)
-                                    for i in D)
-        yield "\ngeneral\n"
-        yield "\n".join(f"y{i}.{j}.{t}" for t in range(1, T+1)
-                                        for i, j in A)
-
-    return "\n".join(_gen())
+    """Model variables."""    
+    yield "bounds\n"
+    yield "\n".join(f"y{i}.{j}.{t} >= 0" for t in range(1, T+1)
+                                            for i, j in A)
+    yield "\nbinaries\n"
+    yield "\n".join(f"x{i}.{t}" for t in range(1, T+1)
+                                for i in D)
+    yield ""
+    yield "\n".join(f"z{i}.{t}" for t in range(1, T+1)
+                                for i in D)
+    yield "\ngeneral\n"
+    yield "\n".join(f"y{i}.{j}.{t}" for t in range(1, T+1)
+                                    for i, j in A)
 
 
 if __name__ == "__main__":
     print(objective_function(), end="\n\n")
     print("st", end="\n\n")
-    print(eq_nb_wt(), end="\n\n")
-    print(eq_wt_flow(), end="\n\n")
-    print(eq_repear(), end="\n\n")
-    print(eq_repeared(), end="\n\n")
-    print(vars(), end="\n\n")
+    print("\n".join(eq_nb_wt()), end="\n\n")
+    print("\n".join(eq_wt_flow()), end="\n\n")
+    print("\n".join(eq_repear()), end="\n\n")
+    print("\n".join(eq_repeared()), end="\n\n")
+    print("\n".join(vars()), end="\n\n")
     print("end")
--- models/model.py
+++ models/model.py
@@ -27,60 +27,51 @@ T = sum(p.values())
 def objective_function():
     """The objective function."""
     R = sum(r.values()) * T
-    return "min " + " ".join(f"- {r[i]} z{i}.{t}" for i in D for t in range(1, T+1)) + f" + {R}"
+    return ("min "
+            + " ".join(f"- {r[i]} z{i}.{t}" for i in D for t in range(1, T+1))
+            + f" + {R}")
                                                   
-
 
 def eq_nb_wt(id=2):
     """Equations to ensure that the maximum number of WT is respected.
     """
-    def _gen():
-        for t in range(1, T+1):
-            yield (f"c{id}.{t}: "
-                   + " + ".join(f"x{i}.{t}" for i in D)
-                   + f" <= {sum(q.values())}")
-
-    return "\n".join(_gen())
+    for t in range(1, T+1):
+        yield (f"c{id}.{t}: "
+                + " + ".join(f"x{i}.{t}" for i in D)
+                + f" <= {sum(q.values())}")
 
 
 def eq_repear(id=4):
     """Equations to ensure that every node gets the exacly amount of work it needs.
        Setting the z_i^t properly.
     """
-    def _gen():
-        for c, i in enumerate(D):
-            for t in range(1, T+1):
-                yield (f"c{id}.{t}.{c}: {p[i]} z{i}.{t}"
-                       + "".join(f" - x{i}.{tt}" for tt in range(1, t+1))
-                       + " <= 0")
-
-    return "\n".join(_gen())
+    for c, i in enumerate(D):
+        for t in range(1, T+1):
+            yield (f"c{id}.{t}.{c}: {p[i]} z{i}.{t}"
+                    + "".join(f" - x{i}.{tt}" for tt in range(1, t+1))
+                    + " <= 0")
 
 
 def eq_repeared(id=5):
     """Equations to ensure that all nodes are cleaned at the last period."""
-    return f"c{id}: " + " + ".join(f"z{i}.{T}" for i in D) + f" = {len(D)}"
+    yield f"c{id}: " + " + ".join(f"z{i}.{T}" for i in D) + f" = {len(D)}"
 
 
 def vars():
     """Model variables."""
-    
-    def _gen():
-        yield "\nbinaries\n"
-        yield "\n".join(f"x{i}.{t}" for t in range(1, T+1)
-                                    for i in D)
-        yield ""
-        yield "\n".join(f"z{i}.{t}" for t in range(1, T+1)
-                                    for i in D)
-
-    return "\n".join(_gen())
+    yield "binaries\n"
+    yield "\n".join(f"x{i}.{t}" for t in range(1, T+1)
+                                for i in D)
+    yield ""
+    yield "\n".join(f"z{i}.{t}" for t in range(1, T+1)
+                                for i in D)
 
 
 if __name__ == "__main__":
     print(objective_function(), end="\n\n")
     print("st", end="\n\n")
-    print(eq_nb_wt(), end="\n\n")
-    print(eq_repear(), end="\n\n")
-    print(eq_repeared(), end="\n\n")
-    print(vars(), end="\n\n")
+    print("\n".join(eq_nb_wt()), end="\n\n")
+    print("\n".join(eq_repear()), end="\n\n")
+    print("\n".join(eq_repeared()), end="\n\n")
+    print("\n".join(vars()), end="\n\n")
     print("end")
