e70e2fd39e41b8692f6451a5931c9d52545b2e2f
Thiago Jobson <thiagojobson@gmail.com>
2022-10-31 16:09:30 +0100

    Fix a failing test

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -5,10 +5,7 @@ from optlis import generate_instance
 
 def test_generate_instance():
     """Tests the function to generate a problem instance."""
-    # Generates an homogeneous risk hexagonal instance
     inst = generate_instance(size=(3, 2), nb_teams=1, seed=0)
     assert np.array_equal(inst.depots, np.array([0]))
     assert np.array_equal(inst.tasks, np.array([1, 2, 3, 4, 5, 6, 7]))
-    for i in inst.tasks:
-        inst.nodes[i]["r"] = 0.5
-    assert list(inst.time_periods)[-1] == 30
+    assert list(inst.time_periods)[-1] == 54
