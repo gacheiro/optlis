e2a733e9e1623f969e60507d610feefa21fccc21
Thiago Jobson <thiagojobson@gmail.com>
2022-12-02 14:53:33 +0100

    Version 0.2.0-alpha

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="optlis",
-    version="0.1.0",
+    version="0.2.0-alpha",
     author="Thiago J. Barbalho",
     description=__doc__,
     packages = ["optlis"],
@@ -13,6 +13,7 @@ setup(
     },
     include_package_data=False,
     install_requires=[
+        "invoke>=1.7.3",
         "jupyter>=1.0.0",
         "matplotlib>=3.3.4",
         "networkx>=2.5",
